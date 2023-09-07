ec3c4b050b06cb6e60d899bc158a07e6d6ca4b9e
Mạnh Đạt <dat.tm24@gmail.com>
2023-09-07 19:37:39 +0700

    first

---
+++ src/ExceptionDemo.java
@@ -0,0 +1,23 @@
+public class ExceptionDemo {
+
+    public static void main(String[] args) {
+        uncheckedNullPointer();
+    }
+
+    private static void uncheckedException() throws RuntimeException {
+        throw new RuntimeException("Hello World");
+    }
+
+    private static void checkedException() throws Exception {
+        throw new Exception("Hello World");
+    }
+
+    private static void error() throws Error {
+        throw new Error("Hello World");
+    }
+
+    private static void uncheckedNullPointer() throws NullPointerException {
+        throw new NullPointerException("Hello World");
+    }
+
+}
